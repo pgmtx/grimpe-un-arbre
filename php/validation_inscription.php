a4bbad97783dfa9ce8fe01d3e9d55192590820d8
pgmtx <programutox@disroot.org>
2025-01-03 15:56:00 +0100

    Revu la taille des panneaux d'inscription/connexion et remplacé l'entrée radio par un select

--- php/validation_inscription.php
+++ php/validation_inscription.php
@@ -2,19 +2,22 @@
   $nom_utilisateur = $_POST["nom_utilisateur"];
   echo "<p>Bienvenue $nom_utilisateur !</p>";
 
-  $niveau = $_POST["niveau_grimpe"];
+  $niveau = $_POST["selection_niveau"];
 
   switch ($niveau) {
-    case 'débutant':
+    case '1':
       echo "<p>Ne t'inquiète pas ça va bien se passer.</p>";
       break;
-    
-    case 'intermédiaire':
+
+    case '2':
       echo "<p>Tu as déjà effectué deux trois grimpades. Améliorons ça !</p>";
       break;
-    
-    default:
+
+    case '3':
       echo "<p>T'y es fortiche, tu pourras aider les autres !</p>";
       break;
+
+    default:
+      throw new Exception("Niveau invalide : $niveau", 1);
   }
 ?>
