1f0aab3a6eb752c2f39d57d055a8e457f441f698
pgmtx <programutox@disroot.org>
2025-01-03 21:27:03 +0100

    Ajouté vérificateur de mot de passe en JS

--- js/gestion_mdp.js
+++ js/gestion_mdp.js
@@ -1,6 +1,6 @@
 function changerAffichageMotDePasse() {
-  let input = document.getElementById("mot_de_passe");
-  input.type = (input.type === "password") ? "text" : "password";
+  let mot_de_passe = document.getElementById("mot_de_passe");
+  mot_de_passe.type = (mot_de_passe.type === "password") ? "text" : "password";
 }
 
 function compterTypesCaractere(texte) {
@@ -27,12 +27,40 @@ function verifierForceMotDePasse() {
     return false;
   }
 
-  const taille_minimum = 8;
-  const mot_de_passe = document.getElementById("mot_de_passe").value;
-  if (mot_de_passe.length < taille_minimum) {
+  const longueur_minimum = 8;
+  const mot_de_passe_entre = document.getElementById("mot_de_passe").value;
+  if (mot_de_passe_entre.length < longueur_minimum) {
     return false;
   }
 
-  let [majuscules, minuscules, chiffres] = compterTypesCaractere(mot_de_passe);
+  let [majuscules, minuscules, chiffres] = compterTypesCaractere(mot_de_passe_entre);
   return (majuscules > 0) && (minuscules > 0) && (chiffres > 0);
 }
+
+function verifierValidite(id, condition) {
+  let element = document.getElementById(id);
+  if (condition) {
+    element.classList.remove("invalide");
+    element.classList.add("valide");
+  } else {
+    element.classList.remove("valide");
+    element.classList.add("invalide");
+  }
+}
+
+function gererAffichageCriteresMotDePasse() {
+  let mot_de_passe = document.getElementById("mot_de_passe");
+
+  mot_de_passe.onfocus = () => {
+    document.getElementById("message").style.display = "block";
+  };
+
+  mot_de_passe.onkeyup = () => {
+    verifierValidite("minuscule", mot_de_passe.value.match(/[a-z]/g));
+    verifierValidite("majuscule", mot_de_passe.value.match(/[A-Z]/g));
+    verifierValidite("chiffre", mot_de_passe.value.match(/[0-9]/g));
+    verifierValidite("longueur", mot_de_passe.value.length >= 8);
+  };
+}
+
+gererAffichageCriteresMotDePasse();
