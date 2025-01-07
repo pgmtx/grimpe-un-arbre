a12b0971e2aa92b04b3b66f355d826e40866c997
pgmtx <programutox@disroot.org>
2025-01-07 19:27:03 +0100

    Restructuré code pour valider la connexion

--- php/validation_connexion.php
+++ php/validation_connexion.php
@@ -1,25 +1,6 @@
 <?php
-    try {
-      verifier_validite_connexion();
-    } catch (Exception $e) {
-      echo "<script type=\"text/javascript\">
-        function revenir_page_connexion() {
-        window.location.href = \"../connexion.html\";
-        alert(\"Erreur: {$e->getMessage()}\");
-        };
-        revenir_page_connexion();
-      </script>";
-    }
-?>
-
-<?php
-function verifier_validite_connexion() {
-  if (!isset($_POST["nom_utilisateur"])) {
-    throw new Exception("Impossible de valider la connexion si vous n'êtes pas connecté.");
-  }
-
+function obtenir_compte_correspondant($nom_utilisateur) {
   $configs = include('config.php');
-  $nom_utilisateur = $_POST["nom_utilisateur"];
 
   $pdo_source = "mysql:host={$configs['host']};dbname={$configs['dbname']}";
   $pdo = new PDO($pdo_source, $configs['username'], $configs['password']);
@@ -28,17 +9,41 @@ function verifier_validite_connexion() {
   $requete->execute();
   $resultat = $requete->fetchAll();
 
-  if (sizeof($resultat) == 0) {
+  if (sizeof($resultat) === 0) {
     throw new Exception("L'utilisateur '$nom_utilisateur' n'existe pas.");
   }
 
+  return $resultat[0];
+}
+
+function verifier_validite_connexion() {
+  // A lieu si on essaye d'accéder à cette page sans être connecté
+  if (!isset($_POST["nom_utilisateur"])) {
+    throw new Exception("Impossible de valider la connexion si vous n'êtes pas connecté.");
+  }
+
+  $nom_utilisateur = $_POST["nom_utilisateur"];
+  $compte = obtenir_compte_correspondant($nom_utilisateur);
+
   $mot_de_passe = $_POST["mot_de_passe"];
-  $mot_de_passe_attendu = $resultat[0]['mot_de_passe'];
+  $mot_de_passe_attendu = $compte['mot_de_passe'];
 
   if ($mot_de_passe !== $mot_de_passe_attendu) {
     throw new Exception("Mauvais mot de passe.");
   }
 
   echo "Bienvenue $nom_utilisateur !";
+}
+
+try {
+  verifier_validite_connexion();
+} catch (Exception $e) {
+  echo "<script type=\"text/javascript\">
+    function revenir_page_connexion() {
+    window.location.href = \"../connexion.html\";
+    alert(\"Erreur: {$e->getMessage()}\");
+    };
+    revenir_page_connexion();
+  </script>";
 }
 ?>
