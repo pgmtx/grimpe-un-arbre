0268b4bcce85d6d4918895487024842ce3963259
pgmtx <programutox@disroot.org>
2025-01-07 09:55:54 +0100

    Ajouté système de confirmation de mot de passe

--- js/gestion_mdp.js
+++ js/gestion_mdp.js
@@ -33,6 +33,11 @@ function verifierForceMotDePasse() {
     return false;
   }
 
+  const mot_de_passe_confirme = document.getElementById("confirmation_mdp").value;
+  if (mot_de_passe_confirme !== mot_de_passe_entre) {
+    return false;
+  }
+
   let [majuscules, minuscules, chiffres] = compterTypesCaractere(mot_de_passe_entre);
   return (majuscules > 0) && (minuscules > 0) && (chiffres > 0);
 }
@@ -60,6 +65,11 @@ function gererAffichageCriteresMotDePasse() {
     verifierValidite("majuscule", mot_de_passe.value.match(/[A-Z]/g));
     verifierValidite("chiffre", mot_de_passe.value.match(/[0-9]/g));
     verifierValidite("longueur", mot_de_passe.value.length >= 8);
+  };
+
+  let confirmation_mot_de_passe = document.getElementById("confirmation_mdp");
+  confirmation_mot_de_passe.onkeyup = () => {
+    verifierValidite("memes_mdp", mot_de_passe.value === confirmation_mot_de_passe.value);
   };
 }
 
