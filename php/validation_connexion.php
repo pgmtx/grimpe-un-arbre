30b6dcb2c93d4ef761bacadaa005cbf2d990759e
pgmtx <programutox@disroot.org>
2025-01-07 11:26:32 +0100

    Réindenté les fichiers php

--- php/validation_connexion.php
+++ php/validation_connexion.php
@@ -1,5 +1,5 @@
 <?php
-  $nom_utilisateur = $_POST["nom_utilisateur"];
-  assert(!empty(nom_utilisateur), "Le nom d'utilisateur est vide.");
-  echo "Content de te revoir " . $nom_utilisateur . " !";
+$nom_utilisateur = $_POST["nom_utilisateur"];
+assert(!empty(nom_utilisateur), "Le nom d'utilisateur est vide.");
+echo "Content de te revoir " . $nom_utilisateur . " !";
 ?>
--- php/validation_inscription.php
+++ php/validation_inscription.php
@@ -1,23 +1,23 @@
 <?php
-  $nom_utilisateur = $_POST["nom_utilisateur"];
-  echo "<p>Bienvenue $nom_utilisateur !</p>";
+$nom_utilisateur = $_POST["nom_utilisateur"];
+echo "<p>Bienvenue $nom_utilisateur !</p>";
 
-  $niveau = $_POST["selection_niveau"];
+$niveau = $_POST["selection_niveau"];
 
-  switch ($niveau) {
-    case '1':
-      echo "<p>Ne t'inquiète pas ça va bien se passer.</p>";
-      break;
+switch ($niveau) {
+  case '1':
+    echo "<p>Ne t'inquiète pas ça va bien se passer.</p>";
+    break;
 
-    case '2':
-      echo "<p>Tu as déjà effectué deux trois grimpades. Améliorons ça !</p>";
-      break;
+  case '2':
+    echo "<p>Tu as déjà effectué deux trois grimpades. Améliorons ça !</p>";
+    break;
 
-    case '3':
-      echo "<p>T'y es fortiche, tu pourras aider les autres !</p>";
-      break;
+  case '3':
+    echo "<p>T'y es fortiche, tu pourras aider les autres !</p>";
+    break;
 
-    default:
-      throw new Exception("Niveau invalide : $niveau", 1);
-  }
+  default:
+    throw new Exception("Niveau invalide : $niveau", 1);
+}
 ?>
