e0f3c49e432704bdf02fd6cb67c8db8509a9ca39
pgmtx <programutox@disroot.org>
2025-01-16 22:22:05 +0100

    Ajouté affichage de la date de création du compte

--- php/flux/infos.php
+++ php/flux/infos.php
@@ -28,6 +28,28 @@
     <p>Niveau : {$compte['niveau']}</p>
     <p>Arbres grimpés : {$compte['arbres_grimpes']}</p>
   ";
+
+  function obtenir_mois_francais($mois) {
+    $mois_anglais = array(
+      "January", "February", "March", "April", "May", "June", "July",
+      "August", "September", "October", "November", "Décember"
+    );
+    $mois_francais = array(
+      "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet",
+      "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+    );
+    assert(count($mois_anglais) == count($mois_francais));
+
+    $indice = array_search($mois, $mois_anglais);
+    return $mois_francais[$indice];
+  }
+
+  $date_brute = substr($compte['date_creation'], 0, strlen('YYYY-MM-DD'));
+  $date_creation = DateTime::createFromFormat('Y-m-d', $date_brute);
+  $sortie = explode(" ", $date_creation->format('j F Y')); // jour Mois année
+  $mois = obtenir_mois_francais($sortie[1]);
+  $date = "{$sortie[0]} $mois {$sortie[2]}";
+  echo "<p>Compte créé le : $date</p>"
   ?>
   </body>
 </html>
