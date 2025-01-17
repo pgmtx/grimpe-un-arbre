7e1deb828b987ce47d3d934eda4132f7bd8ec3f1
pgmtx <programutox@disroot.org>
2025-01-17 16:30:38 +0100

    Achevé le système de réinitialisation de mot de passe

--- php/affichage_erreur.php
+++ php/affichage_erreur.php
@@ -1,11 +1,11 @@
 <?php
-function executerAvecErreurs($fonction) {
+function executerAvecErreurs($fonction, $lien_redirection='./connexion.php') {
   try {
     $fonction();
   } catch (Exception $e) {
     echo "<script type=\"text/javascript\">
       function revenir_page_connexion() {
-      window.location.href = \"./connexion.php\";
+      window.location.href = \"$lien_redirection\";
       alert(\"Erreur: {$e->getMessage()}\");
       };
       revenir_page_connexion();
--- php/reinitialiser_mdp.php
+++ php/reinitialiser_mdp.php
@@ -1,20 +1,28 @@
 <?php 
-require('requetes.php');
+function valider_reinitialisation() {
+  require('requetes.php');
 
-if (isset($_COOKIE['identifiant'])) {
+  if (isset($_COOKIE['identifiant'])) {
+    header('Location: ./connexion.php');
+    die();
+  }
+
+  $nom_utilisateur = $_POST['nom_utilisateur'];
+  $compte = obtenir_compte_correspondant($nom_utilisateur);
+  $mot_de_passe = $compte['mot_de_passe'];
+  $nouveau_mot_de_passe = $_POST["mot_de_passe"];
+
+  if ($mot_de_passe == $nouveau_mot_de_passe) {
+    throw new Exception("Le nouveau mot de passe est identique au précédent");
+  }
+
+  $sql = "UPDATE comptes SET mot_de_passe = '$nouveau_mot_de_passe' WHERE comptes.identifiant = '$nom_utilisateur'";
+  faire_requete_sql($sql);
+
   header('Location: ./connexion.php');
   die();
 }
 
-$nom_utilisateur = $_POST['nom_utilisateur'];
-$compte = obtenir_compte_correspondant($nom_utilisateur);
-$mot_de_passe = $compte['mot_de_passe'];
-$nouveau_mot_de_passe = $_POST["mot_de_passe"];
-
-if ($mot_de_passe == $nouveau_mot_de_passe) {
-  throw new Exception("Le nouveau mot de passe est identique au précédent");
-}
-
-$sql = "UPDATE comptes SET mot_de_passe = '$nouveau_mot_de_passe' WHERE comptes.identifiant = '$nom_utilisateur'";
-faire_requete_sql($sql);
+require('affichage_erreur.php');
+executerAvecErreurs(function() { valider_reinitialisation(); }, '../reinitialiser_mdp.html');
 ?>
